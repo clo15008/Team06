e0fa4b2bcf1f552e912888dc71e8c4a6b36c16bd
Garrett Cloud <clo15008@byui.edu>
2018-03-02 16:57:06 -0700

    Merge branch 'master' of https://github.com/clo15008/Team06
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/example/blank/RecipeLoader.java

--- app/src/main/java/com/example/blank/RecipeLoader.java
+++ app/src/main/java/com/example/blank/RecipeLoader.java
@@ -13,6 +13,7 @@ import java.net.URL;
 public class RecipeLoader {
     public void load()
     {
+        //
         try {
             HttpResponse<JsonNode> response = Unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients=apples%2Cflour%2Csugar")
                     .header("X-Mashape-Key", "ittcmgzIz1mshRfHT4GfOzDIgM4rp1bdJ59jsnI7kl8mVjgxCw")
