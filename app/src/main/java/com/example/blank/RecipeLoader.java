4cd403e2b3882d192f8f0ba02a01aba49f0a5cf4
Garrett Cloud <clo15008@byui.edu>
2018-03-06 16:42:59 -0700

    Merge branch 'master' of https://github.com/clo15008/Team06
    # Conflicts:
    #	app/src/main/java/com/example/blank/RecipeLoader.java

--- app/src/main/java/com/example/blank/RecipeLoader.java
+++ app/src/main/java/com/example/blank/RecipeLoader.java
@@ -10,23 +10,64 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class RecipeLoader {
 
-    Recipe recipe;
-    JSONObject jsonObject;
+    private Recipe recipe;
+    private JSONObject jsonObject;
+
+    public void loadRecipe() {
+        try {
+            // Create a stream to the URL
+            URL url = new URL("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients/")
+                    .header("X-Mashape-Key", "ittcmgzIz1mshRfHT4GfOzDIgM4rp1bdJ59jsnI7kl8mVjgxCw")
+                    .header("X-Mashape-Host", "spoonacular-recipe-food-nutrition-v1.p.mashape.com");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            // Read all data from the website into a single string
+            String line = "";
+            String allLines = "";
+            do {
+                line = reader.readLine();
+                if (line != null) {
+                    allLines += line;
+                }
+            }
+            while (line != null);
+
+            // Create the EarthquakeList object from the JSON data
+            Gson gson = new Gson();
+            recipe = gson.fromJson(allLines, Recipe.class);
+        }
+        catch (MalformedURLException murle) {
+            System.out.println(murle.getMessage());
+        }
+        catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
+    }
 
     public void load()
     {
         try {
-            HttpResponse<JsonNode> response = Unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients=apples%2Cflour%2Csugar")
+            HttpResponse<JsonNode> response = Unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients/")
                     .header("X-Mashape-Key", "ittcmgzIz1mshRfHT4GfOzDIgM4rp1bdJ59jsnI7kl8mVjgxCw")
                     .header("X-Mashape-Host", "spoonacular-recipe-food-nutrition-v1.p.mashape.com")
                     .asJson();
+
+
+            // THIS CODE MIGHT BE AN ALTERNATE FORM OF THE ABOVE REQUEST
+            /*HttpResponse<Recipe> r = Unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients/" + "milk").asObject(Recipe.class);
+            Recipe re = r.getBody();
+            System.out.println(re);*/
 
             jsonObject = response.getBody().getObject();
 
@@ -41,8 +82,8 @@ public class RecipeLoader {
             e.printStackTrace();
 
         }
-
-
-
+    }
+    public Recipe getRecipeList() {
+        return recipe;
     }
 }
