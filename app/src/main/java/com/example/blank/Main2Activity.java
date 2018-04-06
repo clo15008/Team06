19b0c8dc1a07d807eb41cc559cf68ef3e3df4733
Katya <https://github.com/katerynapalchykova/cs246>
2018-04-06 17:36:24 -0600

    Merge branch 'master' of https://github.com/clo15008/Team06
    # Conflicts:
    #	app/src/main/java/com/example/blank/Main2Activity.java

--- app/src/main/java/com/example/blank/Main2Activity.java
+++ app/src/main/java/com/example/blank/Main2Activity.java
@@ -251,7 +251,7 @@ public class Main2Activity extends AppCompatActivity {
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 //gcloud API - ittcmgzIz1mshRfHT4GfOzDIgM4rp1bdJ59jsnI7kl8mVjgxCw
                 // kj API - BBB93pKWHNmshVQ2JNR0STYwPj7Xp1hdsyMjsnJbdNPTkS63hu
-                connection.setRequestProperty("X-Mashape-Key", "BBB93pKWHNmshVQ2JNR0STYwPj7Xp1hdsyMjsnJbdNPTkS63hu");
+                connection.setRequestProperty("X-Mashape-Key", "Z4VortkhmBmshnQP8ZDVuCWD6c6mp183oC1jsnT5HTCulZ3BDF");
                 connection.setRequestProperty("X-Mashape-Host", "spoonacular-recipe-food-nutrition-v1.p.mashape.com");
                 connection.setRequestMethod("GET");
 
@@ -274,6 +274,12 @@ public class Main2Activity extends AppCompatActivity {
 
                 Log.i("test", allLines);
 
+                RecipeInfo recipeInfo = gson.fromJson(allLines, RecipeInfo.class);
+                recipesInfo.add(recipeInfo);
+                url_list.add(recipeInfo.getImageURL());
+                recipe_title.add(recipeInfo.getTitle());
+                recipe_likes.add(recipeInfo.getLikes());
+
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (ProtocolException e) {
@@ -281,12 +287,7 @@ public class Main2Activity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-            RecipeInfo recipeInfo = gson.fromJson(allLines, RecipeInfo.class);
-            recipesInfo.add(recipeInfo);
-            url_list.add(recipeInfo.getImageURL());
-            recipe_title.add(recipeInfo.getTitle());
-            recipe_likes.add(recipeInfo.getLikes());
+            
 
             return null;
         }
