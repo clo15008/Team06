dc07a84b6f83407669ee115dd160b286e3a59b4d
Katya <https://github.com/katerynapalchykova/cs246>
2018-04-06 22:36:41 -0600

    Merge branch 'master' of https://github.com/clo15008/Team06
    # Conflicts:
    #	app/src/main/java/com/example/blank/Main2Activity.java

--- app/src/main/java/com/example/RecipeReady/IntroActivity.java
+++ app/src/main/java/com/example/RecipeReady/IntroActivity.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 /**
  * Author: Katya
  * This activity is designed to present the first screen when a user opens the app.
- * It explains how to get a recipe by inputting names of given ingredients from user's kitchen.
+ * It explains how to get a recipe by inputting names of given ingredients from a user's kitchen.
  */
 
 public class IntroActivity extends AppCompatActivity {
